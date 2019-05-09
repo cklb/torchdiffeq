80e4950ae439774d67dbda9b3c56387da341f1f2
Stefan Ecklebe <stefan.ecklebe@tu-dresden.de>
2019-05-09 10:48:20 +0200

    Extended rk4 routine for parallel batch solving

--- torchdiffeq/_impl/rk_common.py
+++ torchdiffeq/_impl/rk_common.py
@@ -72,7 +72,11 @@ def rk4_step_func(func, t, dt, y, k1=None):
 def rk4_alt_step_func(func, t, dt, y, k1=None):
     """Smaller error with slightly more compute."""
     if k1 is None: k1 = func(t, y)
-    k2 = func(t + dt / 3, tuple(y_ + dt * k1_ / 3 for y_, k1_ in zip(y, k1)))
-    k3 = func(t + dt * 2 / 3, tuple(y_ + dt * (k1_ / -3 + k2_) for y_, k1_, k2_ in zip(y, k1, k2)))
-    k4 = func(t + dt, tuple(y_ + dt * (k1_ - k2_ + k3_) for y_, k1_, k2_, k3_ in zip(y, k1, k2, k3)))
-    return tuple((k1_ + 3 * k2_ + 3 * k3_ + k4_) * (dt / 8) for k1_, k2_, k3_, k4_ in zip(k1, k2, k3, k4))
+    if t.dim() > 0:
+        dt_ = dt.view(dt.shape[0], 1, 1).expand(k1[0].shape)
+    else:
+        dt_ = dt
+    k2 = func(t + dt / 3, tuple(y_ + dt_ * k1_ / 3 for y_, k1_ in zip(y, k1)))
+    k3 = func(t + dt * 2 / 3, tuple(y_ + dt_ * (k1_ / -3 + k2_) for y_, k1_, k2_ in zip(y, k1, k2)))
+    k4 = func(t + dt, tuple(y_ + dt_ * (k1_ - k2_ + k3_) for y_, k1_, k2_, k3_ in zip(y, k1, k2, k3)))
+    return tuple((k1_ + 3 * k2_ + 3 * k3_ + k4_) * (dt_ / 8) for k1_, k2_, k3_, k4_ in zip(k1, k2, k3, k4))
